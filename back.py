0f58e9d3bec61ba960ccd9363e73abce642acb7e
Mariam Adel <mariamadel9420@gmail.com>
2023-05-18 09:01:37 +0300

    send result correctly to result page but still have an empty result label

--- back.py
+++ back.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,jsonify
 import json
 from pymongo import MongoClient
+
 
 app = Flask(__name__)
 
@@ -13,7 +14,6 @@ structure_col = db['Structure']
 chain_col = db['Chain']
 experimental_col = db['Experimental']
 
-
 @app.route('/data')
 def get_data():
     cursor1 = structure_col.find()
@@ -25,82 +25,54 @@ def get_data():
     return render_template('home.html', data1=data1, data2=data2, data3=data3)
 
 
+@app.route('/process_query', methods=['POST'])
+def process_query():
+    collection_name = request.form['collection']
+    query_type = request.form['query_type'].lower()  # Convert query_type to lowercase
+    query_str = request.form['query']
+    query = json.loads(query_str)
+
+    if collection_name == 'Structure':
+        collection = structure_col
+    elif collection_name == 'Chain':
+        collection = chain_col
+    elif collection_name == 'Experimental':
+        collection = experimental_col
+    else:
+        return render_template('result.html', query_type=query_type, query=query, data=[])
+
+    if query_type == 'find':
+        cursor = collection.find(query)
+        data = [document for document in cursor]
+    elif query_type == 'insert':
+        result = collection.insert_one(query)
+        data = [{'inserted_id': str(result.inserted_id)}]
+    elif query_type == 'update':
+        result = collection.update_one(query['filter'], query['update'])
+        data = [{'matched_count': result.matched_count, 'modified_count': result.modified_count}]
+    elif query_type == 'delete':
+        result = collection.delete_one(query)
+        data = [{'deleted_count': result.deleted_count}]
+    elif query_type == 'aggregate':
+        cursor = collection.aggregate(query)
+        data = [document for document in cursor]
+    else:
+        data = []
+
+    return render_template('result.html', query_type=query_type, query=query, data=data)
+
+
+# ...
+
 @app.route('/result')
 def show_result():
-    result = request.args.get('result')
-    return render_template('result.html', result=result)
+    data = request.args.get('data')
+    print(data)  # Add this line to check the value of data
+    result_list = data.split(',') if data else []
+    return render_template('result.html', data=result_list)
 
-
-@app.route('/data', methods=['GET', 'POST'])
-def get_data_route():
-    if request.method == 'POST':
-        # Get the submitted data from the form
-        collection = request.form.get('collection')
-        query_type = request.form.get('query_type')
-        query = request.form.get('query')
-
-        # Convert the query string to a JSON object
-        query_object = json.loads(query)
-
-        # Perform the query on the specified collection based on the query type
-        if collection == 'Structure':
-            if query_type == 'Find':
-                result = structure_col.find(query_object)
-            elif query_type == 'Insert':
-                structure_col.insert_one(query_object)
-                result = "Data inserted successfully"
-            elif query_type == 'Update':
-                # Update operation
-                update_data = json.loads(request.form['update_data'])
-                result = collection.update_many(query, {'$set': update_data})
-                result = f'Updated {result.modified_count} documents'
-            elif query_type == 'Delete':
-                # delete operation
-                result = collection.delete_many(query)
-                result = f'Deleted {result.deleted_count} documents'
-            else:
-                result = None
-
-        elif collection == 'Chain':
-            if query_type == 'Find':
-                result = chain_col.find(query_object)
-            elif query_type == 'Insert':
-                chain_col.insert_one(query_object)
-                result = "Data inserted successfully"
-            elif query_type == 'Update':
-                # Update operation
-                update_data = json.loads(request.form['update_data'])
-                result = collection.update_many(query, {'$set': update_data})
-                result = f'Updated {result.modified_count} documents'
-            elif query_type == 'Delete':
-                result = collection.delete_many(query)
-                result = f'Deleted {result.deleted_count} documents'
-            else:
-                result = None
-
-        elif collection == 'Experimental':
-            if query_type == 'Find':
-                result = experimental_col.find(query_object)
-            elif query_type == 'Insert':
-                experimental_col.insert_one(query_object)
-                result = "Data inserted successfully"
-            elif query_type == 'Update':
-                # Update operation
-                update_data = json.loads(request.form['update_data'])
-                result = collection.update_many(query, {'$set': update_data})
-                result = f'Updated {result.modified_count} documents'
-            elif query_type == 'Delete':
-                # delete operation
-                result = collection.delete_many(query)
-                result = f'Deleted {result.deleted_count} documents'
-            else:
-                result = None
-
-        # Redirect to the result page after processing the form data
-        return redirect(url_for('show_result', result=result))
-
-    return render_template('result.html')
+# ...
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
