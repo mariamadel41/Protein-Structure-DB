96fceb736587f542b168a0557cb4c3da68b1aa50
Mariam Adel <mariamadel9420@gmail.com>
2023-05-22 01:48:15 +0300

    find and upgate queries

--- mongo_setup.py
+++ mongo_setup.py
@@ -1,7 +1,9 @@
-# import pandas as pd
-# from pymongo import MongoClient
-# from pymongo.errors import DuplicateKeyError
-
+import pandas as pd
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+from pprint import pprint
+import csv
+import re
 # # shrink csv files to 5000 records:
 # dups_df = pd.read_csv('data//dups.csv').head(5000)
 # seq_df = pd.read_csv('data//seq.csv').head(12000)
@@ -12,15 +14,17 @@
 # seq_df = pd.read_csv('data//seq_updated.csv')
 
 
-# # Create a new MongoClient and database
-# client = MongoClient('127.0.0.1:27017')
+# Create a new MongoClient and database
+client = MongoClient('127.0.0.1:27017')
 
-# db = client['ProteinStructure']
+db = client['ProteinStructure']
 
-# # Create the three collections
-# structure_col = db['Structure']
-# chain_col = db['Chain']
-# experimental_col = db['Experimental']
+# Create the three collections
+structure_col = db['Structure']
+chain_col = db['Chain']
+experimental_col = db['Experimental']
+
+
 
 # # Loop through the rows of dups_df and insert each row as a document in the structure_col collection
 # for index, row in dups_df.iterrows():
@@ -68,7 +72,7 @@
 #     experimental_col.insert_one(document)
 
 
-# # Update the Structure collection with chainId
+# # Update the Structure collection with chainId to make (Reference Relation)
 # updated_count = db.Structure.update_many({}, {"$set": {"chainId": []}}).modified_count
 # print(f"Updated {updated_count} documents in Structure collection with empty chainId")
 # # Iterate over the Chain collection and add the chainId to the corresponding Structure documents
@@ -88,3 +92,58 @@
 
 # experimental_df = pd.DataFrame(list(experimental_col.find()))
 # experimental_df.to_csv('data//experemintal.csv', index=False)
+
+
+# #Find Query:
+# cursor = structure_col.find({"classification": "DNA-RNA HYBRID"})
+# for document in cursor:
+#     pprint(document)
+#     print('\n')
+
+# cursor2 = chain_col.find({"chainId": "H"})
+# for document in cursor2:
+#     pprint(document)
+#     print('\n')
+
+
+# cursor3 = experimental_col.find({"resolution": {"$gt" : 2.0}})
+# for document in cursor3:
+#     pprint(document)
+#     print('\n')
+
+# cursor4 = structure_col.find({"residueCount": {"$gte" : 300}})
+# for document in cursor4:
+#     pprint(document)
+#     print('\n')
+
+# cursor5 = structure_col.find({"$and" : [ {"macromoleculeType" : "DNA"} , {"residueCount": {"$lt" : 50}}]})
+# for document in cursor5:
+#     pprint(document)
+#     print('\n')
+
+
+
+
+
+# Updating the records
+result = experimental_col.update_many(
+    {"crystallizationMethod": {"$regex": re.compile(r'^\s*$', re.IGNORECASE)}},
+    {"$set": {"crystallizationMethod": "nan"}}
+)
+
+print(f"Number of documents matched: {result.matched_count}")
+print(f"Number of documents updated: {result.modified_count}")
+
+# Retrieve documents from the collection
+documents = list(experimental_col.find())
+
+# Define the fieldnames for the CSV file
+fieldnames = ['_id', 'structureId', 'experimentalTechnique', 'resolution', 'crystallizationMethod', 'crystallizationTempK']
+
+# Save documents to a CSV file
+with open('/home/mariam/Downloads/Protein/data/experemintal.csv', 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(documents)
+
+print("Collection saved to experimental.csv successfully.")
