21f92de3707bb5ea80f5ea5df6f945915e5692cd
Mariam Adel <mariamadel9420@gmail.com>
2023-05-22 18:21:14 +0300

    process the whole query and add query_input.txt

--- back.py
+++ back.py
@@ -39,16 +39,49 @@ def process_query():
             collection = experimental_col
         else:
             return render_template('result.html', query_type=query_type, query=query, data=[])
-
         if query_type == 'find':
-            cursor = collection.find(query)
-            data = [document for document in cursor]
+            conditions = [query]
+            sub_condition = {}  # Initialize sub_condition variable
+            for sub_query in conditions:
+                field = sub_query.get('field')
+                value = sub_query.get('value')
+                operator = sub_query.get('operator')
+
+                if operator == 'equal':
+                    sub_condition = {field: {'$eq': value}}
+                elif operator == 'greater_than':
+                    sub_condition = {field: {'$gt': value}}
+                elif operator == 'greater_than_or_equal':
+                    sub_condition = {field: {'$gte': value}}
+                elif operator == 'less_than':
+                    sub_condition = {field: {'$lt': value}}
+                elif operator == 'less_than_or_equal':
+                    sub_condition = {field: {'$lte': value}}
+                elif operator == 'and':
+                    sub_condition = {'$and': value}
+                elif operator == 'or':
+                    sub_condition = {'$or': value}
+
+                conditions.append(sub_condition)
+
+            query = {"$and": conditions}  # Use "$and" operator for combining conditions
+            results = collection.find(query)
+            data = '\n'.join([str(r) for r in results])
         elif query_type == 'insert':
             result = collection.insert_one(query)
             data = [{'inserted_id': str(result.inserted_id)}]
         elif query_type == 'update':
             result = collection.update_one(query['filter'], query['update'])
             data = [{'matched_count': result.matched_count, 'modified_count': result.modified_count}]
+        elif query_type == 'index':
+            index_fields = query['index_fields']
+            index_order = query['index_order']
+            index_name = collection.create_index([(index_fields, index_order)])
+            data = [{'index_name': index_name}]
+
+    # Fetch the collection with the new index
+            cursor = collection.find().hint(index_name)
+            data = [document for document in cursor]
         elif query_type == 'delete':
             result = collection.delete_one(query)
             data = [{'deleted_count': result.deleted_count}]
@@ -61,9 +94,6 @@ def process_query():
     except Exception as e:
         print(e)
         return render_template('error.html', error_message=str(e))
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
